a9b8a5be3acaf70f9cd6ec14209525018ae67eb1
rakeshsukla53 <rakesh.sukla53@gmail.com>
2017-08-18 16:36:27 -0400

    populate jquery properly

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,36 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/sergiocruz.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourses(response.courses.completed)
+    }
+  });
 
+  function addCourses(coursesList) {
+    var $badges = $('#badges');
+    coursesList.forEach( function(course) {
+
+      var $course = $('<div />', {
+        class: 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        href: course.url,
+        target: '_blank',
+        class: 'btn btn-primary',
+        text: 'See Course'
+      }).appendTo($course);
+      
+    });
+  }
 });
